7278102fde64eb15ccafa9243b563cd3a9d1648d
Andy Miles <akmiles@icloud.com>
2024-10-05 06:46:11 -0700

    ai db

--- src/pawl/db_analysis.py
+++ src/pawl/db_analysis.py
@@ -9,15 +9,15 @@ collection = chroma_client.get_or_create_collection(name="my_collection")
 
 collection.upsert(
     documents=[
-        "read a guide to touring vineyards in france",
+        "read a guide to vineyards in france",
         "watch a tv show about portugal",
-        "visit alaska"
+        "catch salmon in alaska"
     ],
     ids=["id1", "id2", "id3"]
 )
 
 results = collection.query(
-    query_texts=["see grapes harvested"],
+    query_texts=["learn about grapes being harvested"],
     n_results=2
 )
 
